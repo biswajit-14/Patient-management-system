eb29fbd4378b3d91eedd00be4a7a70b6f978f233
Biswajit <biswajit.swain@zysk.tech>
2024-09-03 07:54:04 +0530

    Added passkey verification for Admin access

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -1,34 +1,59 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     AlertDialog,
     AlertDialogAction,
-    AlertDialogCancel,
     AlertDialogContent,
     AlertDialogDescription,
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
-    AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import {
     InputOTP,
     InputOTPGroup,
-    InputOTPSeparator,
     InputOTPSlot,
 } from "@/components/ui/input-otp"
+import { decryptKey, encryptKey } from '@/lib/utils'
 
 
 const PasskeyModal = () => {
-    const router = useRouter()
+    const router = useRouter();
+    const path = usePathname();
     const [open, setOpen] = useState(true);
     const [passkey, setPasskey] = useState('');
     const [error, setError] = useState('');
 
-    const validatePasskey = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => { }
+    const encryptedKey = typeof window !== 'undefined' ? window.localStorage.getItem('accessKey') : null;
+
+    useEffect(() => {
+        const accessKey = encryptedKey && decryptKey(encryptedKey);
+
+        if (path) {
+            if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+                setOpen(false);
+                router.push('/admin')
+            } else {
+                setOpen(true);
+            }
+        }
+    }, [encryptedKey])
+
+    const validatePasskey = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+            const encryptedKey = encryptKey(passkey);
+
+            localStorage.setItem('accessKey', encryptedKey);
+            setOpen(false);
+        } else {
+            setError('Invalid passkey')
+        }
+    }
 
     const closeModal = () => {
         setOpen(false)

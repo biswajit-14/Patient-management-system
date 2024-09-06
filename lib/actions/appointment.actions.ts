599afac49afb7ce44445dab7ed4cf8cad63a595b
Biswajit <biswajit.swain@zysk.tech>
2024-09-06 18:27:28 +0530

    Started with Admin dashboard page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,12 +1,13 @@
 "use server";
 
+import { Appointment } from "@/types/appwrite.types";
 import {
     DATABASE_ID,
     databases,
     APPOINTMENT_COLLECTION_ID
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -32,6 +33,46 @@ export const getAppointment = async (appointmentId: string) => {
         )
 
         return parseStringify(appointment);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getRecentAppointmentsList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [Query.orderDesc("createdAt")]
+        )
+
+        const initialCounts = {
+            scheduleCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0,
+        }
+
+        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+            if (appointment.status === 'scheduled') {
+                acc.scheduleCount++;
+            }
+            if (appointment.status === 'pending') {
+                acc.pendingCount++;
+            }
+            if (appointment.status === 'cancelled') {
+                acc.cancelledCount++;
+            }
+
+            return acc;
+        }, initialCounts)
+
+        const data = {
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents
+        }
+
+        return parseStringify(data);
     } catch (error) {
         console.log(error)
     }

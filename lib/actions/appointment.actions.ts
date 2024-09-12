d0ace18ac2eaf67388e2dc47bd7033cf50f417e0
Biswajit <biswajit.swain@zysk.tech>
2024-09-12 09:16:28 +0530

    WIP: Admin page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -38,42 +38,69 @@ export const getAppointment = async (appointmentId: string) => {
     }
 }
 
-export const getRecentAppointmentsList = async () => {
+export const getRecentAppointmentList = async () => {
     try {
         const appointments = await databases.listDocuments(
             DATABASE_ID!,
             APPOINTMENT_COLLECTION_ID!,
-            [Query.orderDesc("createdAt")]
-        )
+            [Query.orderDesc("$createdAt")]
+        );
+
+        // const scheduledAppointments = (
+        //   appointments.documents as Appointment[]
+        // ).filter((appointment) => appointment.status === "scheduled");
+
+        // const pendingAppointments = (
+        //   appointments.documents as Appointment[]
+        // ).filter((appointment) => appointment.status === "pending");
+
+        // const cancelledAppointments = (
+        //   appointments.documents as Appointment[]
+        // ).filter((appointment) => appointment.status === "cancelled");
+
+        // const data = {
+        //   totalCount: appointments.total,
+        //   scheduledCount: scheduledAppointments.length,
+        //   pendingCount: pendingAppointments.length,
+        //   cancelledCount: cancelledAppointments.length,
+        //   documents: appointments.documents,
+        // };
 
         const initialCounts = {
-            scheduleCount: 0,
+            scheduledCount: 0,
             pendingCount: 0,
             cancelledCount: 0,
-        }
+        };
 
-        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
-            if (appointment.status === 'scheduled') {
-                acc.scheduleCount++;
-            }
-            if (appointment.status === 'pending') {
-                acc.pendingCount++;
-            }
-            if (appointment.status === 'cancelled') {
-                acc.cancelledCount++;
-            }
-
-            return acc;
-        }, initialCounts)
+        const counts = (appointments.documents as Appointment[]).reduce(
+            (acc, appointment) => {
+                switch (appointment.status) {
+                    case "scheduled":
+                        acc.scheduledCount++;
+                        break;
+                    case "pending":
+                        acc.pendingCount++;
+                        break;
+                    case "cancelled":
+                        acc.cancelledCount++;
+                        break;
+                }
+                return acc;
+            },
+            initialCounts
+        );
 
         const data = {
             totalCount: appointments.total,
             ...counts,
-            documents: appointments.documents
-        }
+            documents: appointments.documents,
+        };
 
         return parseStringify(data);
     } catch (error) {
-        console.log(error)
+        console.error(
+            "An error occurred while retrieving the recent appointments:",
+            error
+        );
     }
-}
+};

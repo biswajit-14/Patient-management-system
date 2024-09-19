91937ba6c6cbeda80627328745cbce69480148a2
Biswajit <biswajit.swain@zysk.tech>
2024-09-19 08:39:49 +0530

    Added Sentry - A debugger tool

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://cd8cc5719e381476f00b428c303a5943@o4507971757146112.ingest.de.sentry.io/4507971761930320",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
